058c04dda52c26c9286446db49784bc8cfa76f8c
vinicius-developer <viniciusilvav1@hotmail.com>
2022-08-19 18:14:52 +0000

    add dolar quote period WIP

--- src/main/java/com/api/bcb/currency/exchange/dollar/quote/period/ds/ResultDolarQuote.java
+++ src/main/java/com/api/bcb/currency/exchange/dollar/quote/period/ds/ResultDolarQuote.java
@@ -1,5 +1,53 @@
 package com.api.bcb.currency.exchange.dollar.quote.period.ds;
 
+import java.math.BigDecimal;
+
 public class ResultDolarQuote {
-    
+
+    private BigDecimal purchaseQuote;
+
+    private BigDecimal saleQuote;
+
+    private String hourQuote;
+
+    public ResultDolarQuote(
+        BigDecimal purchaseQuote, 
+        BigDecimal saleQuote, 
+        String hourQuote ) {
+        this.purchaseQuote = purchaseQuote;
+
+        this.saleQuote = saleQuote;
+
+        this.hourQuote = hourQuote;
+
+    }
+
+    public BigDecimal getPurchaseQuote() {
+        return this.purchaseQuote;
+    }
+
+    public BigDecimal getSaleQuote() {
+        return this.saleQuote;
+    }
+
+    public String hourQuote() {
+        return this.hourQuote;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("{\n")
+            .append("\"purchaseQuote\": ")
+            .append(this.purchaseQuote)
+            .append(", \n")
+            .append("\"sellQuote\": ")
+            .append(this.saleQuote)
+            .append(", \n")
+            .append("\"hourQuote\": ")
+            .append(this.hourQuote)
+            .append("\n}")
+            .toString();
+            
+    }
+        
 }
--- src/main/java/com/api/bcb/currency/exchange/dollar/quote/period/ds/SearchResultDolarQuotePeriod.java
+++ src/main/java/com/api/bcb/currency/exchange/dollar/quote/period/ds/SearchResultDolarQuotePeriod.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.api.bcb.currency.commom.api.search.SearchResult;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class SearchResultDolarQuotePeriod extends SearchResult 
@@ -16,6 +18,28 @@ public class SearchResultDolarQuotePeriod extends SearchResult
         JsonObject jsonObject = new Gson().fromJson(from, JsonObject.class);
 
         this.setContext(jsonObject.get("@odata.context").getAsString());
+
+        JsonArray jsonArray = jsonObject.get("value").getAsJsonArray();
+
+        this.setListResultDolarQuotes(jsonArray);
+
+    }
+
+    public void setListResultDolarQuotes(JsonArray array) {
+
+        for (JsonElement element : array) {
+
+            JsonObject object = element.getAsJsonObject();
+
+            ResultDolarQuote resultDolarQuote = new ResultDolarQuote(
+                object.get("cotacaoCompra").getAsBigDecimal(), 
+                object.get("cotacaoVenda").getAsBigDecimal(), 
+                object.get("dataHoraCotacao").getAsString()
+            );
+
+            this.listResultDolarQuotes.add(resultDolarQuote);
+            
+        }
 
     }
 
