c50e6b419be92e3e63c360475b932261ad170648
vinicius-developer <viniciusilvav1@hotmail.com>
2022-08-22 19:45:56 +0000

    factories changes

--- src/main/java/com/api/bcb/currency/exchange/factory/ExchangeTaxesFactory.java
+++ src/main/java/com/api/bcb/currency/exchange/factory/ExchangeTaxesFactory.java
@@ -1,11 +1,16 @@
 package com.api.bcb.currency.exchange.factory;
 
+import com.api.bcb.currency.commom.api.currency.ContryCurrency;
 import com.api.bcb.currency.exchange.currency.Currency;
 import com.api.bcb.currency.exchange.currency.CurrencyInterface;
 import com.api.bcb.currency.exchange.dollar.quote.day.DollarQuoteDay;
 import com.api.bcb.currency.exchange.dollar.quote.period.DollarQuotePeriod;
 import com.api.bcb.currency.exchange.dollar.quote.day.DollarQuoteDayInterface;
 import com.api.bcb.currency.exchange.dollar.quote.period.DollarQuotePeriodInterface;
+import com.api.bcb.currency.exchange.reportcard.quote.day.ReportCardQuoteDay;
+import com.api.bcb.currency.exchange.reportcard.quote.day.ReportCardQuoteDayInterface;
+import com.api.bcb.currency.exchange.reportcard.quote.period.ReportCardQuotePeriod;
+import com.api.bcb.currency.exchange.reportcard.quote.period.ReportCardQuotePeriodInterface;
 
 /**
  * Fabrica responsável por encapsular a lógica para a criação 
@@ -27,5 +32,18 @@ public class ExchangeTaxesFactory implements ExchangeTaxesInterface {
     public DollarQuotePeriodInterface dollarQuotePeriod(String start, String end) {
         return new DollarQuotePeriod(start, end);
     }
+
+    @Override
+    public ReportCardQuoteDayInterface reportCardQuoteDay(
+        ContryCurrency currency, String date) {
+        return new ReportCardQuoteDay(currency, date);
+    }
+
+    @Override
+    public ReportCardQuotePeriodInterface reportCardQuotePeriod(
+        ContryCurrency currency, String start, String end) {
+        return new ReportCardQuotePeriod(currency, start, end);
+    }
+
     
 }
--- src/main/java/com/api/bcb/currency/exchange/factory/ExchangeTaxesInterface.java
+++ src/main/java/com/api/bcb/currency/exchange/factory/ExchangeTaxesInterface.java
@@ -1,8 +1,11 @@
 package com.api.bcb.currency.exchange.factory;
 
+import com.api.bcb.currency.commom.api.currency.ContryCurrency;
 import com.api.bcb.currency.exchange.currency.CurrencyInterface;
 import com.api.bcb.currency.exchange.dollar.quote.day.DollarQuoteDayInterface;
 import com.api.bcb.currency.exchange.dollar.quote.period.DollarQuotePeriodInterface;
+import com.api.bcb.currency.exchange.reportcard.quote.day.ReportCardQuoteDayInterface;
+import com.api.bcb.currency.exchange.reportcard.quote.period.ReportCardQuotePeriodInterface;
 
 public interface ExchangeTaxesInterface {
 
@@ -11,5 +14,9 @@ public interface ExchangeTaxesInterface {
     DollarQuoteDayInterface dollarQuoteDay(String date);
 
     DollarQuotePeriodInterface dollarQuotePeriod(String start, String end);
+
+    ReportCardQuoteDayInterface reportCardQuoteDay(ContryCurrency currency, String date);
+
+    ReportCardQuotePeriodInterface reportCardQuotePeriod(ContryCurrency currency, String start, String end);
 
 }
