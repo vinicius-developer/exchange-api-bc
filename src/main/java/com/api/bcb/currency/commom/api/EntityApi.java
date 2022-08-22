e0f03aa2ed32cacb6a8dfcecd47a44005cdaefb7
vinicius-developer <viniciusilvav1@hotmail.com>
2022-08-22 19:37:57 +0000

    EntityApiInterface to EntityApi

---
+++ src/main/java/com/api/bcb/currency/commom/api/EntityApi.java
@@ -0,0 +1,15 @@
+package com.api.bcb.currency.commom.api;
+
+/**
+ * Classe utilizada para definiciar um contrato com as classes filhas 
+ * 
+ * O método getRequestParam tem a intenção de retornar o path com os parãmetros
+ * corretos para o endpoint. É intessante que esse método também não possa ser chamdo 
+ * fora dos escopos da classe.
+ * 
+ */
+public abstract class EntityApi {
+
+    protected abstract String getRequestParams();
+    
+}
